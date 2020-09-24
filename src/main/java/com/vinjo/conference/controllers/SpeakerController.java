45a25e3d2783643647595986addd65e23336b228
Vinay Joshi <vinay01joshi@gmail.com>
2020-09-24 15:15:50 +0530

    Fixing in Json Serializtion

--- src/main/java/com/vinjo/conference/controllers/SpeakerController.java
+++ src/main/java/com/vinjo/conference/controllers/SpeakerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/speaker")
+@RequestMapping("/api/v1/speakers")
 public class SpeakerController {
 
     @Autowired
--- src/main/java/com/vinjo/conference/models/Session.java
+++ src/main/java/com/vinjo/conference/models/Session.java
@@ -1,9 +1,12 @@
 package com.vinjo.conference.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name="sessions")
+@JsonIgnoreProperties({ "hibernateLazyInitializer","handler" })
 public class Session {
 
     @Id
